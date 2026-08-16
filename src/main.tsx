ef4983ca1701e4674a274a8f2d525e9b730c1e3d
Hendrik Louis Mahdi <hendrikmahdi@gmail.com>
2026-08-17 02:49:29 +0700

    feat(ui): self-host Inter Variable and JetBrains Mono Variable

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import '@fontsource-variable/inter/index.css';
+import '@fontsource-variable/jetbrains-mono/index.css';
 import './assets/main.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.tsx';
